d15b0674499a52ed93e899af3d85d8d811cc0570
Jan Kornienko <kor.jan@outlook.com>
2021-05-30 16:21:59 +0200

    Boxes

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
-	<h1 class="title is-1">Rick and Morty Gallery</h1>
-	<div v-for="char in charactersObject" :key="char">
-		<figure class="image is-128x128">
-			<img :src="char.image" alt="">
-		</figure>
-		<h5 class="title is-5">{{ char.name }}</h5>
-		<h5 class="title is-5">{{ char.status }} - {{ char.species }}</h5>
-		<h5 class="title is-5">{{ char.gender }}</h5>
-		<h5 class="title is-5">{{ char.origin.name }}</h5>
+	<div class="container is-max-desktop is-flex is-flex-direction-column">
+		<h1 class="title is-1">Rick and Morty Gallery</h1>
+		<a class="button is-primary" @click="prev()">Previous</a>
+		<h5 class="title is-5">Page: {{ this.page }}</h5>
+		<a class="button is-primary" @click="next()">Next</a>
+		<div class="columns" v-for="char in charactersObject" :key="char">
+			<div class="column box is-one-fifth">
+				<figure class="image is-128x128">
+					<img :src="char.image" alt="">
+				</figure>
+				<h5 class="title is-5">{{ char.name }}</h5>
+				<h5 class="title is-5">{{ char.status }} - {{ char.species }}</h5>
+				<h5 class="title is-5">{{ char.gender }}</h5>
+				<h5 class="title is-5">{{ char.origin.name }}</h5>
+			</div>
+		</div>
 	</div>
-	<a class="button is-primary" @click="prev()">Previous</a>
-	<a class="button is-primary" @click="next()">Next</a>
 </template>
 
 <script>
@@ -23,7 +28,8 @@
 				url: 'https://rickandmortyapi.com/api/character',
 				nextUrl: '',
 				prevUrl: '',
-				charactersObject: []
+				charactersObject: [],
+				page: 1,
 			};
 		},
 		created() {
@@ -35,19 +41,19 @@
 					this.charactersObject = response.results;
 					this.nextUrl = response.info.next;
 					this.prevUrl = response.info.prev;
-					console.log(this.charactersObject);
-					console.log(this.nextUrl);
 				});
 			},
 			next() {
 				if(this.nextUrl != null) {
 					this.url = this.nextUrl;
+					this.page++;
 					this.load();
 				};
 			},
 			prev() {
 				if(this.prevUrl != null) {
 					this.url = this.prevUrl;
+					this.page--;
 					this.load();
 				};
 			}
