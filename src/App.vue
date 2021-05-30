245b595c966034c2613277619e2da79c5b6c9d46
Jan Kornienko <kor.jan@outlook.com>
2021-05-30 11:09:19 +0200

    Pages

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,12 @@
 			<img :src="char.image" alt="">
 		</figure>
 		<h5 class="title is-5">{{ char.name }}</h5>
-		<h5 class="title is-5">{{ char.status }}</h5>
-		<h5 class="title is-5">{{ char.species }}</h5>
+		<h5 class="title is-5">{{ char.status }} - {{ char.species }}</h5>
 		<h5 class="title is-5">{{ char.gender }}</h5>
 		<h5 class="title is-5">{{ char.origin.name }}</h5>
 	</div>
+	<a class="button is-primary" @click="prev()">Previous</a>
+	<a class="button is-primary" @click="next()">Next</a>
 </template>
 
 <script>
@@ -20,14 +21,36 @@
 		data() {
 			return {
 				url: 'https://rickandmortyapi.com/api/character',
+				nextUrl: '',
+				prevUrl: '',
 				charactersObject: []
 			};
 		},
 		created() {
-			$.get(this.url, (response) => {
-				this.charactersObject = response.results;
-				console.log(this.charactersObject);
-			});
+			this.load();
+		},
+		methods: {
+			load() {
+				$.get(this.url, (response) => {
+					this.charactersObject = response.results;
+					this.nextUrl = response.info.next;
+					this.prevUrl = response.info.prev;
+					console.log(this.charactersObject);
+					console.log(this.nextUrl);
+				});
+			},
+			next() {
+				if(this.nextUrl != null) {
+					this.url = this.nextUrl;
+					this.load();
+				};
+			},
+			prev() {
+				if(this.prevUrl != null) {
+					this.url = this.prevUrl;
+					this.load();
+				};
+			}
 		}
 	};
 </script>
