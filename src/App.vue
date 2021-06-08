7ec6299025b91f3e4cf7f2b0c29f95f0cc8d9be4
Jan Kornienko <kor.jan@outlook.com>
2021-06-08 16:06:35 +0200

    Search

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
 	<div class="container is-max-desktop">
 		<h1 class="title is-1">Rick and Morty Gallery</h1>
-		<div class="columns">
-			<a class="column button is-primary" @click="prev()">Previous</a>
-			<h5 class="column title is-5">Page: {{ this.page }}</h5>
-			<a class="column button is-primary" @click="next()">Next</a>
-		</div>
-		<div class="columns is-3 is-flex is-flex-wrap-wrap">
-			<div class="column is-one-third box" v-for="char in charactersObject" :key="char">
+		<div v-if="characters != null">
+			<input class="input is-primary" type="text" placeholder="Search" v-model="search" @input="load(1)">
+			<div class="select is-primary">
+				<select v-model="status">
+					<option key="0" label="Alive" value="alive"></option>
+					<option key="1" label="Dead" value="dead"></option>
+					<option key="2" label="Unknown" value="unknown"></option>
+				</select>
+			</div>
+			<div class="box" v-for="char in characters.results" :key="char">
 				<figure class="image is-128x128">
-					<img :src="char.image" alt="">
+					<img :src="char.image">
 				</figure>
-				<h5 class="title is-5">{{ char.name }}</h5>
-				<h5 class="title is-5">{{ char.status }} - {{ char.species }}</h5>
-				<h5 class="title is-5">{{ char.gender }}</h5>
-				<h5 class="title is-5">{{ char.origin.name }}</h5>
+				<div>
+					<h5 class="title is-5">{{ char.name }}</h5>
+					<h5 class="title is-5">{{ char.species }} - {{ char.gender }}</h5>
+					<h5 class="title is-5">{{ char.status }}</h5>
+					<h5 class="title is-5">{{ char.origin.name }}</h5>
+				</div>
+			</div>
+			<div class="pagination">
+				<ul class="pagination-list">
+					<li class="pagination-link" :class="{ 'is-current': page == currentPage }" v-for="page in characters.info.pages" :key="page" @click="load(page)">{{ page }}</li>
+				</ul>
 			</div>
 		</div>
 	</div>
@@ -28,36 +38,25 @@
 		data() {
 			return {
 				url: 'https://rickandmortyapi.com/api/character',
-				nextUrl: '',
-				prevUrl: '',
-				charactersObject: [],
-				page: 1,
+				characters: null,
+				status: null,
+				loading: true,
+				currentPage: 1,
+				search: ''
 			};
 		},
 		created() {
-			this.load();
+			this.load(1);
 		},
 		methods: {
-			load() {
-				$.get(this.url, (response) => {
-					this.charactersObject = response.results;
-					this.nextUrl = response.info.next;
-					this.prevUrl = response.info.prev;
+			load(page) {
+				$.get(this.url, { name: this.search, page: page, status: this.status } , response => {
+					this.characters = response;
+					this.loading = false;
+					this.currentPage = page;
+				}).fail(() => {
+					this.characters = null;
 				});
-			},
-			next() {
-				if(this.nextUrl != null) {
-					this.url = this.nextUrl;
-					this.page++;
-					this.load();
-				};
-			},
-			prev() {
-				if(this.prevUrl != null) {
-					this.url = this.prevUrl;
-					this.page--;
-					this.load();
-				};
 			}
 		}
 	};
