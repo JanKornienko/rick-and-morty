944cf364de829f12ded198681b6a484939d44dbc
Jan Kornienko <kor.jan@outlook.com>
2021-06-09 22:42:15 +0200

    Columns

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
-	<div class="container is-max-desktop">
+	<div class="container">
 		<h1 class="title is-1">Rick and Morty Gallery</h1>
 		<div v-if="characters != null">
-			<input class="input is-primary" type="text" placeholder="Search" v-model="search" @input="load(1)">
-			<div class="select is-primary">
-				<select v-model="status">
-					<option selected key="0" label="All" value=""></option>
-					<option key="1" label="Alive" value="alive"></option>
-					<option key="2" label="Dead" value="dead"></option>
-					<option key="3" label="Unknown" value="unknown"></option>
-				</select>
+			<div>
+				<input class="input is-primary" type="text" placeholder="Search" v-model="search" @input="load(1)">
+				<div class="select is-primary">
+					<select v-model="status" placeholder="Status">
+						<option selected key="0" label="All" value=""></option>
+						<option key="1" label="Alive" value="alive"></option>
+						<option key="2" label="Dead" value="dead"></option>
+						<option key="3" label="Unknown" value="unknown"></option>
+					</select>
+				</div>
 			</div>
-			<div class="box" v-for="char in characters.results" :key="char">
-				<figure class="image is-128x128">
-					<img :src="char.image">
-				</figure>
-				<div>
-					<h5 class="title is-5">{{ char.name }}</h5>
-					<h5 class="title is-5">{{ char.species }} - {{ char.gender }}</h5>
-					<h5 class="title is-5">{{ char.status }}</h5>
-					<h5 class="title is-5">{{ char.origin.name }}</h5>
+			<div class="is-flex is-flex-wrap-wrap is-justify-content-center columns">
+				<div class="box column is-one-quarter mx-3" v-for="char in characters.results" :key="char">
+					<figure class="image is-128x128">
+						<img class="photo" :src="char.image">
+					</figure>
+					<div>
+						<h5 class="title is-5">{{ char.name }}</h5>
+						<h5 class="title is-5">{{ char.species }} - {{ char.gender }}</h5>
+						<h5 class="title is-5">{{ char.status }}</h5>
+						<h5 class="title is-5">{{ char.origin.name }}</h5>
+					</div>
 				</div>
 			</div>
 			<div class="pagination">
@@ -41,7 +45,6 @@
 				url: 'https://rickandmortyapi.com/api/character',
 				characters: null,
 				status: null,
-				loading: true,
 				currentPage: 1,
 				search: ''
 			};
@@ -53,7 +56,6 @@
 			load(page) {
 				$.get(this.url, { name: this.search, page: page, status: this.status } , response => {
 					this.characters = response;
-					this.loading = false;
 					this.currentPage = page;
 				}).fail(() => {
 					this.characters = null;
